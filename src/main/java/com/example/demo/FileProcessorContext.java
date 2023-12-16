a2e26573f25c9272969d6944d14f0b2b1ba64b39
manoj1995madushanka <madushankamanoj414@gmail.com>
2023-12-16 12:23:07 +0530

    implement file processor context

--- src/main/java/com/example/demo/FileProcessorContext.java
+++ src/main/java/com/example/demo/FileProcessorContext.java
@@ -1,4 +1,13 @@
 package com.example.demo;
 
 public class FileProcessorContext {
+    private FileProcessor fileProcessor;
+
+    public void setFileProcessor(FileProcessor fileProcessor) {
+        this.fileProcessor = fileProcessor;
+    }
+
+    public void processFile(String filePath) {
+        fileProcessor.processFile(filePath);
+    }
 }
